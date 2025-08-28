1ffc2c71fad109afce8602a6dd22fb6da4ed409c
jasper <jdelaat1@gmail.com>
2025-08-28 17:40:20 +0200

    Ook ingesteld op thuis ip

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=%!)hivhwx!@$_dobtz6cqvr3v3q5bgs=ihqyp9u6bihl&tt&2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.35"]
 
 
 # Application definition
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8001",
     "http://127.0.0.1:8001",
+    "http:192.168.1.35"
 ]
 
 ROOT_URLCONF = 'backend_project.urls'
